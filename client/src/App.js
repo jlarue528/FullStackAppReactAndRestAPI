683610811dd7a168746db93ee753fcc6c83914b0
Jennifer LaRue <jennifer@photoshelter.com>
2022-08-03 10:35:16 -0400

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { use } from '../../api/routes';
+import { useEffect } from 'react';
 
 function App() {
+
+  let courseList = [];
+
+  useEffect(() => {
+    const apiURL = 'http://localhost:5000/api/courses'
+    fetch(apiURL)
+      .then((res) => res.json())
+      .then((courses) => courses.push(courseList))
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {courseList => {
+          <ul>
+            <li>{courseList}</li>
+          </ul>
+        }};
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
