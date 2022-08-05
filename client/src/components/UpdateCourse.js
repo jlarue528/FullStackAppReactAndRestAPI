ec916bd4f4c447ecc752479c574cfbcf4c95dead
Jennifer LaRue <jennifer@photoshelter.com>
2022-08-05 15:32:00 -0400

    updates

--- client/src/components/UpdateCourse.js
+++ client/src/components/UpdateCourse.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Link, useNavigate, useParams } from 'react-router-dom';
+import { Link, useParams, useNavigate } from 'react-router-dom';
 
 const UpdateCourse = () => {
     const navigate = useNavigate();
--- client/src/components/UserSignIn.js
+++ client/src/components/UserSignIn.js
@@ -1,4 +1,4 @@
-import { NavLink, useNavigate } from 'react-router-dom';
+import { NavLink, useNavigate, Link } from 'react-router-dom';
 
 const UserSignIn = () => {
 
@@ -13,7 +13,7 @@ const UserSignIn = () => {
                     <input id="emailAddress" name="emailAddress" type="email" value=""></input>
                     <label htmlFor="password">Password</label>
                     <input id="password" name="password" type="password" value=""></input>
-                    <button className="button" type="submit">Sign In</button><button className="button button-secondary" onClick={() => {navigate('/api/courses')}}>Cancel</button>
+                    <button className="button" type="submit">Sign In</button><Link to="/api/courses"><button className="button button-secondary" onClick={() => {navigate('/api/courses')}}>Cancel</button></Link>
                 </form>
                 <p>Don't have a user account? Click here to <NavLink to="/signup">sign up</NavLink>!</p>
         </div>
