5526524bccdd4701e39a705d8f3616f341b586c7
Jennifer LaRue <jennifer@photoshelter.com>
2022-08-05 10:50:34 -0400

    debug help

--- client/src/components/UpdateCourse.js
+++ client/src/components/UpdateCourse.js
@@ -1,50 +1,83 @@
-// import { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { Consumer } from './context';
+import { useState, useEffect, useParams, useRef } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
-const UpdateCourse = (props) => {
-  
-    return (
-        <Consumer>
-            { context => {
-                const updateCourseData = () => {
+const UpdateCourse = () => {
+    const [ course, getCourse ] = useState([]);
     
-                }
-                const submitUpdateCourse = () => {
-            
-                }
-                 return (
-                 <div className="wrap">
-                 <h2>Update Course</h2>
-                 <form>
-                     <div className="main--flex">
-                         <div>
-                             <label for="courseTitle">Course Title</label>
-                             <input id="courseTitle" name="courseName" type="text" value={context.course.title}></input>
- 
-                             <p>By Joe Smith</p>
- 
-                             <label for="courseDescription">Course Description</label>
-                             <textarea id="courseDescription" name="courseDescription">{context.course.description}</textarea>
-                         </div>
-                         <div>
-                             <label for="estimatedTime">Estimated Time</label>
-                             <input id="estimatedTime" name="estimatedTime" type="text" value={context.course.estimatedTime}></input>
- 
-                             <label for="materialsNeeded">Materials Needed</label>
-                             <textarea id="materialsNeeded" name="materialsNeeded">{context.course.materialsNeeded}</textarea>
-                         </div>
-                     </div>
-                     <button className="button" type="submit">Update Course</button><Link to="/api/courses"><button className="button button-secondary" onClick={() => {props.history.push('/api/courses')}}>Cancel</button></Link>
-                 </form>
-             </div>
-            );
-        }};
-        </Consumer>
-)};
+    let titleValue = useRef(course.title);
+    let descriptionValue = useRef(course.description);
+    let courseEstimatedTimeValue = useRef(course.estimatedTime);
+    let materialsNeededValue = useRef(course.materialsNeeded);
+
+    const [ title, updateTitle] = useState(titleValue);
+    const [ description, updateDescription ] = useState(descriptionValue);
+    const [ courseEstimatedTime, updateCourseEstimatedTime ] = useState(courseEstimatedTimeValue);
+    const [ materialsNeeded, updateMaterialsNeeded ] = useState(materialsNeededValue);
+
+    const { id } = useParams();
+    const navigate = useNavigate();
+
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/api/courses/${id}`, { method: 'GET' })
+            .then(res => res.json())
+            .then(responseData => {
+                getCourse({course: responseData})
+        })
+        .catch(error => {
+            console.log('Error Fetching Data', error);
+        });
+    }, [id]);
+
+
+    const handleUpdate = () => {
+        fetch(`http://localhost:5000/api/courses/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify({
+                title: title,
+                description: description,
+                estimatedTime: courseEstimatedTime,
+                materialsNeeded: materialsNeeded
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+        .then(response => {
+            console.log(response);
+            updateTitle({title: response.title})
+            updateDescription({description: response.description})
+            updateCourseEstimatedTime({estimatedTime: response.estimatedTime})
+            updateMaterialsNeeded({materialsNeeded: response.materialsNeeded})
+        });
+    };
+
+    return (
+        <div className="wrap">
+        <h2>Update Course</h2>
+        <form>
+            <div className="main--flex">
+                <div>
+                    <label for="courseTitle">Course Title</label>
+                    <input id="courseTitle" name="courseName" type="text" value={course.title}></input>
+
+                    <p>{`By ${course.User.firstName} ${course.User.lastName}`}</p>
+
+                    <label for="courseDescription">Course Description</label>
+                    <textarea id="courseDescription" name="courseDescription">{course.description}</textarea>
+                </div>
+                <div>
+                    <label for="estimatedTime">Estimated Time</label>
+                    <input id="estimatedTime" name="estimatedTime" type="text" value={course.estimatedTime}></input>
+
+                    <label for="materialsNeeded">Materials Needed</label>
+                    <textarea id="materialsNeeded" name="materialsNeeded">{course.materialsNeeded}</textarea>
+                </div>
+            </div>
+            <button className="button" type="submit" onSubmit={handleUpdate}>Update Course</button><Link to="/api/courses"><button className="button button-secondary" onClick={() => navigate("/api/courses")}>Cancel</button></Link>
+        </form>
+    </div>
+   );
+}
 
 export default UpdateCourse;
-
-
-    
-    
