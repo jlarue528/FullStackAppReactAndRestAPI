fdd3af0b789e1dd1aeb1b35e120c8bbacda516bf
Jennifer LaRue <jennifer@photoshelter.com>
2022-08-05 16:45:39 -0400

    Update CreateCourse.js

--- client/src/components/CreateCourse.js
+++ client/src/components/CreateCourse.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default class CreateCourse extends Component {
 
     state = {
-        courses: []
+        course: []
     }
 
   componentDidMount() {
