ebee3ffc7324eb1a42d1775523e49408e9b1f519
Jennifer LaRue <jennifer@photoshelter.com>
2022-08-08 12:27:56 -0400

    provided hardcoded credentials

--- client/src/components/CreateCourse.js
+++ client/src/components/CreateCourse.js
@@ -17,8 +17,12 @@ const CreateCourse = (props) => {
     const [ errors, setErrors ] = useState([]);
 
     const submit = () => {
-        const emailAddress = context.authenticatedUser.username;
-        const password = context.authenticatedUser.password;
+        // const emailAddress = context.authenticatedUser.username;
+        // const password = context.authenticatedUser.password;
+
+        const emailAddress = 'Tester@gmail.com';
+        const password = 'testpassword';
+
 
         //new course data
         const newCourse = {
